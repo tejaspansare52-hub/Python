a534211a8f71c1704479e00352c570b1f7d667f2
Tejas Sunil  Pansare <tejaspansare52@gmail.com>
2025-09-13 22:06:58 +0530

    day 3

--- sept-12.py
+++ sept-12.py
@@ -4,4 +4,17 @@ sum =x+y
 print(sum)
 
 
-
+# check even or odd
+num = 4
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+    
+    
+ a = 2
+b = 3
+a, b = b, a
+print(a, b)
+    
+    
---
+++ sept-13.py
@@ -0,0 +1,29 @@
+  # swap two numbers
+a = 2
+b = 4
+a = a+b
+b = a-b
+a = a-b
+print(a,b)
+
+
+# find the Largest Number
+a = 2;
+b = 4;
+c = 10
+print(max(a,b,c))
+
+
+
+# find the smallest number
+a = 20;
+b = 2;
+c = 32
+print(min(a,b,c))
+
+
+# Area of circle
+x = 7
+area = 3.14 * x * x
+print(area)
+
